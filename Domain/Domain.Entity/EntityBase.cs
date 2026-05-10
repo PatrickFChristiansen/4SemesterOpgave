1e2f4be49b80aba376e1e1643c67d9dee8848741
PatrickFChristiansen <patrickfriis@hotmail.com>
2026-05-10 13:10:34 +0200

    Add project files.

---
+++ Domain/Domain.Entity/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Domain.Entity
+{
+    internal class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
